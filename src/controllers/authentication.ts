c67ea9b3203253e3cfb82197d4b3debcb72193d9
alpolcaymis <alpmis99@gmail.com>
2024-02-24 14:54:45 +0300

    start

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,7 +1,41 @@
-import express from "express";
+import express from 'express';
 
-import { createUser, getUserByEmail } from "db/users";
-import { random, authentication } from "helpers";
+import { getUserByEmail, createUser } from '../db/users';
+import { authentication, random } from '../helpers';
+
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+
+    if (!user) {
+      return res.sendStatus(400);
+    }
+
+    const expectedHash = authentication(user.authentication.salt, password);
+    
+    if (user.authentication.password != expectedHash) {
+      return res.sendStatus(403);
+    }
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+    await user.save();
+
+    res.cookie('ANTONIO-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
 
 export const register = async (req: express.Request, res: express.Response) => {
   try {
@@ -12,7 +46,7 @@ export const register = async (req: express.Request, res: express.Response) => {
     }
 
     const existingUser = await getUserByEmail(email);
-
+  
     if (existingUser) {
       return res.sendStatus(400);
     }
@@ -32,4 +66,4 @@ export const register = async (req: express.Request, res: express.Response) => {
     console.log(error);
     return res.sendStatus(400);
   }
-};
+}
--- src/db/users.ts
+++ src/db/users.ts
@@ -1,8 +1,9 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
+// User Config
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
   email: { type: String, required: true },
+  username: { type: String, required: true },
   authentication: {
     password: { type: String, required: true, select: false },
     salt: { type: String, select: false },
@@ -10,21 +11,13 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-export const UserModel = mongoose.model("User", UserSchema);
+export const UserModel = mongoose.model('User', UserSchema);
 
+// User Actions
 export const getUsers = () => UserModel.find();
 export const getUserByEmail = (email: string) => UserModel.findOne({ email });
-export const getUserBySessionToken = (sessionToken: string) =>
-  UserModel.findOne({
-    "authentication.sessionToken": sessionToken,
-  });
+export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({ 'authentication.sessionToken': sessionToken });
 export const getUserById = (id: string) => UserModel.findById(id);
-export const createUser = (values: Record<string, any>) => {
-  new UserModel(values).save().then((user) => user.toObject());
-};
-export const deleteUserById = (id: string) => {
-  UserModel.findOneAndDelete({ _id: id });
-};
-export const updateUserById = (id: string, values: Record<string, any>) => {
-  UserModel.findByIdAndUpdate(id, values);
-};
+export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject());
+export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
