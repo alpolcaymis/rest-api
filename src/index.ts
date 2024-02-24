60b79b24abb6bb7f6250d38ce6f7f5ebbfa4ad61
alpolcaymis <alpmis99@gmail.com>
2024-02-24 14:03:22 +0300

    24 feb Saturday In coffee

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.use(
   cors({
-    credential: true,
+    credentials: true,
   })
 );
 
@@ -18,3 +19,15 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
+server.listen(8080, () => {
+  console.log("Server running on http://localhost:8080/");
+});
+
+const MONGO_URL =
+  "mongodb+srv://alp:alp@cluster0.vsfetig.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => {
+  console.log(error);
+});
